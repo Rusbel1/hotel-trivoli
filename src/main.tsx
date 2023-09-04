72b0513049d72710fa9c426092d725246be8a30e
rusbel <santosvanegas13@gmail.com>
2023-09-03 22:41:40 -0500

    comint
    s

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ProductCard from './billing/components/productCard/cards';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <div>
+        <ProductCard />
+      </div>
     </BrowserRouter>
   </React.StrictMode>,
 );
